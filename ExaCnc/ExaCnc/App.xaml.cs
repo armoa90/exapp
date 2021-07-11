068f35d389e50af7b7d4299e943a01b15094a375
armoa90 <armoa1590@gmail.com>
2021-07-10 22:24:02 -0400

    exa cnc

---
+++ ExaCnc/ExaCnc/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ExaCnc.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExaCnc
+{
+    public partial class App : Application
+    {
+        #region constructor
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new InicioPage();
+        }
+        #endregion
+        #region Metodos
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+        #endregion Metodos
+    }
+}
